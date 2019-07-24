aad544817abe4981c0fd274d04b4455d11846a1b
Christopher Dent <christopherdent01@gmail.com>
2019-07-24 19:20:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,13 @@
 
 def reverse_each_word(string)
 
-words = string.split #{"I", 'wanna', "go"}
+  words = string.split #{"I", 'wanna', "go"}
 
-rever = words.map do |word|
-  word.reverse
-end
+  rev = words.map do |word|
+    word.reverse
+  end
   
-puts rever
+  rev.join
+
 end
 
